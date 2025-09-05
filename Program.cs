164586715908d37b60b16a74bc1b250fd822a8be
Daevion Maitland <362726697@GoTVDSB.ca>
2025-09-05 08:48:49 -0400

    done

--- Program.cs
+++ Program.cs
@@ -4,16 +4,24 @@
     {
         static void Main(string[] args)
         {
+            //Daevion
+            //this code has letters in it of my initials
             Console.WriteLine("Hello, World i'm Daevion!");
 
             Console.WriteLine(" ");
 
-            Console.Write(" comments are used to leave messages or inform anyone who may use that code");
+            Console.Write(" Comments are used to leave messages to your future self or inform anyone who may use that code later on (eg. a mistake you have to fix.");
             
             Console.WriteLine(" ");
 
-            Console.Write("D");
-            
+            Console.WriteLine("DDDD      M   M");
+            Console.WriteLine("D    D    MM MM ");
+            Console.WriteLine("D    D    MM MM");
+            Console.WriteLine("D    D    M M M");
+            Console.WriteLine("D    D    M   M");
+            Console.WriteLine("D    D    M   M");
+            Console.WriteLine("DDDD      M   M");
+
         }
     }
 }
