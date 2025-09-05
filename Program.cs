f59056293af46b11ff0df39d2525a5dc6a806be4
362726697 <362726697@2260I-L29-015>
2025-09-05 08:27:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Topic_1_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World i'm Daevion!");
+
+            Console.WriteLine(" ");
+
+            Console.Write(" comments are used to leave messages or inform anyone who may use that code");
+            
+            Console.WriteLine(" ");
+
+            Console.Write("D");
+            
+        }
+    }
+}
